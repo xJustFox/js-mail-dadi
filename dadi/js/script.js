09d98dc2f331360ad769d0c15532e7aec81b99cf
xJustFox <dodydaversa@gmail.com>
2023-11-23 14:34:12 +0100

    [Dadi] Created "else if"

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -6,3 +6,12 @@ console.log("Tu stai tirando i dadi:")
 user_num = Math.floor(Math.random() * 6) + 1;
 console.log(user_num)
 
+if(pc_num == user_num){
+    console.log("Pareggio")
+}
+else if(pc_num > user_num){
+    console.log("Ha vinto il computer")
+}
+else{
+    console.log("hai vinto")
+}

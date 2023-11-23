6e4b5fe289663c4857f1568305e89f50dd59a5f8
xJustFox <dodydaversa@gmail.com>
2023-11-23 13:48:34 +0100

    [Mail] Created "for" loop and "if"

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,2 +1,10 @@
 const emails = ["armandorizzi@gmail.com", "mariorossi@gmail.com", "francoesposito@gmail.com"];
 let user_email = prompt("Inserisci la tua email:");
+
+for (let i=0; i<emails.length; i++) {
+    console.log(emails[i]);
+
+    if (user_email == emails[i]) {
+        console.log("Benvenuto nel Sito");
+    }
+}

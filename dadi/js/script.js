ae529cf6dd887f4aba8363920d2479f12f006629
xJustFox <dodydaversa@gmail.com>
2023-11-23 14:33:00 +0100

    [Dadi] "pc_num" "user_num" functions defined

---
+++ dadi/js/script.js
@@ -0,0 +1,8 @@
+console.log("Il computer sta tirando i dadi:")
+let pc_num =  Math.floor(Math.random() * 6) + 1;
+console.log(pc_num)
+
+console.log("Tu stai tirando i dadi:")
+user_num = Math.floor(Math.random() * 6) + 1;
+console.log(user_num)
+

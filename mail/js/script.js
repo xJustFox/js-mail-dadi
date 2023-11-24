e750df8da53edfa270f0f9937af2d4e602c217c8
xJustFox <dodydaversa@gmail.com>
2023-11-24 09:50:57 +0100

    [Mail]Correct exercise

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,10 +1,17 @@
 const emails = ["armandorizzi@gmail.com", "mariorossi@gmail.com", "francoesposito@gmail.com"];
 let user_email = prompt("Inserisci la tua email:");
+let check = false;
 
 for (let i=0; i<emails.length; i++) {
     console.log(emails[i]);
 
     if (user_email == emails[i]) {
-        console.log("Benvenuto nel Sito");
+        check = true;
     }
+}
+
+if (check == true) {
+    console.log("Email presente");
+} else {
+    console.log("Email non presente");
 }
